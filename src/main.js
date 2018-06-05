c0b04e3286902d65f6d10abe942bdb176e598ce0
peterhauer <pethaudi@yahoo.de>
2018-06-05 17:29:21 +0200

    added bars chart

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
-function fetchFromLocalFile(path, toSave){
-    fetch(path)
-        .then(response => JSON.parse(response))
-        .then(json => toSave = json);
+showAppearancePerContinent()
+
+function fetchFromData(name){
+    return fetch("data/" + name + ".json")
+        .then(response => response.json())
+}
+
+async function showAppearancePerContinent(){
+    let data;
+    data = await fetchFromData("AppearancesPerContinent");
+    var arr = new Array();
+    console.log(data)
+    for(i = 0; i < data.length; i++){
+        tmp = data[i].counter
+
+        if(tmp < 1000){
+            arr.push(1)
+        } else {
+            arr.push((tmp / 1000) * 2)
+        }
+    }
+
+    d3.select(".appearancesPerContinent")
+        .selectAll("div")
+        .data(arr)
+            .enter()
+            .append("div")
+            .style("width", function(d) { return d + "px" })
+            .text(function(d) { return d })
 }
