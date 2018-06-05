a25f18aa23aede5469605ca6b4314509ff0d6d75
peterhauer <pethaudi@yahoo.de>
2018-06-05 17:50:25 +0200

    added comments

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 showAppearancePerContinent()
 
+/*
+you always need to make an async funtion to wait for the result of the fetch
+*/
 function fetchFromData(name){
     return fetch("data/" + name + ".json")
         .then(response => response.json())
@@ -8,8 +11,9 @@ function fetchFromData(name){
 async function showAppearancePerContinent(){
     let data;
     data = await fetchFromData("AppearancesPerContinent");
+
+    //writing the data into an array
     var arr = new Array();
-    console.log(data)
     for(i = 0; i < data.length; i++){
         tmp = data[i].counter
 
@@ -20,11 +24,13 @@ async function showAppearancePerContinent(){
         }
     }
 
-    d3.select(".appearancesPerContinent")
-        .selectAll("div")
-        .data(arr)
-            .enter()
-            .append("div")
-            .style("width", function(d) { return d + "px" })
-            .text(function(d) { return d })
+    d3.select(".appearancesPerContinent") //selecting the div
+        .selectAll("div")   //it should take all div (but there are none)
+        .data(arr)  //select datasource
+            .enter()    //this says something like: "if there are none then create"
+            .append("div")  //create divs
+            .style("width", function(d) { return d + "px" }) //styling the div
+            .attr("class", "appearancesPerContinentBar")
+            .text(function(d) { return d }) //setting the text
 }
+
