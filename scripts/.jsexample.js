a1dd2ef54355a26f448f53e9a120e24366ceb30d
peterhauer <pethaudi@yahoo.de>
2018-06-07 21:44:36 +0200

    added and repaired the appearancesPerType bars

--- scripts/.jsexample.js
+++ scripts/.jsexample.js
@@ -6,3 +6,19 @@ function fetchFromData(name) {
     return fetch("src/data/" + name + ".json")
         .then(response => response.json());
 }
+
+//for bars3
+function orderData(xaxis, yaxis){
+    for(i = 0; i < yaxis.length; i++){
+        for(j = 0; j < yaxis.length; j++){
+            if(yaxis[j] < yaxis[i]){
+                tmp = yaxis[i]
+                yaxis[i] = yaxis[j]
+                yaxis[j] = tmp
+                tmp = xaxis[i]
+                xaxis[i] = xaxis[j]
+                xaxis[j] = tmp
+            }
+        }
+    }
+}
--- src/js/appearancesPerType.js
+++ src/js/appearancesPerType.js
@@ -1,17 +1,21 @@
 appearancesPerType()
+appearancesPerTypeTop10()
+
 
 async function appearancesPerType() {
     var data = await fetchFromData("AppearancesPerType")
 
-    var xaxis = new Array()
+    xaxis = new Array()
     for (i = 0; i < data.length; i++) {
         xaxis.push(data[i].name)
     }
 
-    var yaxis = new Array()
+    yaxis = new Array()
     for (i = 0; i < data.length; i++) {
         yaxis.push(data[i].counter)
     }
+
+    orderData(xaxis, yaxis)
 
     var canvas = document.querySelector("#appearancesPerType")
 
@@ -29,11 +33,48 @@ async function appearancesPerType() {
     });
 }
 
+async function appearancesPerTypeTop10(){
+    var data = await fetchFromData("AppearancesPerType")
+    var newxaxis = new Array()
+    var newyaxis = new Array()
+
+    for(i = 0; i < 10; i++){
+        newxaxis.push(xaxis[i])
+        newyaxis.push(yaxis[i])
+    }
+
+    var canvas = document.querySelector("#appearancesPerTypeTOP10")
+
+    var densityData = {
+        label: "appearances per type",
+        data: newyaxis
+    }
+
+    var barChart = new Chart(canvas, {
+        type: 'bar',
+        data: {
+            labels: newxaxis,
+            datasets: [densityData]
+        }
+    });
+}
+
 function fetchFromData(name) {
     return fetch("src/data/" + name + ".json")
         .then(response => response.json());
 }
 
 function orderData(xaxis, yaxis){
-    
+    for(i = 0; i < yaxis.length; i++){
+        for(j = 0; j < yaxis.length; j++){
+            if(yaxis[j] < yaxis[i]){
+                tmp = yaxis[i]
+                yaxis[i] = yaxis[j]
+                yaxis[j] = tmp
+                tmp = xaxis[i]
+                xaxis[i] = xaxis[j]
+                xaxis[j] = tmp
+            }
+        }
+    }
 }
