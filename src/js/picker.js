ecb594f0dfa2d2a4af09df129e71e3dfea88f5e3
peterhauer <pethaudi@yahoo.de>
2018-06-24 19:41:12 +0200

    added picker.js (for picking the icon)

---
+++ src/js/picker.js
@@ -0,0 +1,455 @@
+function getMarker(pokemonid){
+    if(pokemonid == 1){
+       return image001
+   }
+   else if(pokemonid == 2){
+       return image002
+    }
+   else if(pokemonid == 3){
+       return image003
+    }
+   else if(pokemonid == 4){
+       return image004
+    }
+   else if(pokemonid == 5){
+       return image005
+    }
+   else if(pokemonid == 6){
+       return image006
+    }
+   else if(pokemonid == 7){
+       return image007
+    }
+   else if(pokemonid == 8){
+       return image008
+    }
+   else if(pokemonid == 9){
+       return image009
+    }
+   else if(pokemonid == 10){
+       return image010
+    }
+   else if(pokemonid == 11){
+       return image011
+    }
+   else if(pokemonid == 12){
+       return image012
+    }
+   else if(pokemonid == 13){
+       return image013
+    }
+   else if(pokemonid == 14){
+       return image014
+    }
+   else if(pokemonid == 15){
+       return image015
+    }
+   else if(pokemonid == 16){
+       return image016
+    }
+   else if(pokemonid == 17){
+       return image017
+    }
+   else if(pokemonid == 18){
+       return image018
+    }
+   else if(pokemonid == 19){
+       return image019
+    }
+   else if(pokemonid == 20){
+       return image020
+    }
+   else if(pokemonid == 21){
+       return image021
+    }
+   else if(pokemonid == 22){
+       return image022
+    }
+   else if(pokemonid == 23){
+       return image023
+    }
+   else if(pokemonid == 24){
+       return image024
+    }
+   else if(pokemonid == 25){
+       return image025
+    }
+   else if(pokemonid == 26){
+       return image026
+    }
+   else if(pokemonid == 27){
+       return image027
+    }
+   else if(pokemonid == 28){
+       return image028
+    }
+   else if(pokemonid == 29){
+       return image029
+    }
+   else if(pokemonid == 30){
+       return image030
+    }
+   else if(pokemonid == 31){
+       return image031
+    }
+   else if(pokemonid == 32){
+       return image032
+    }
+   else if(pokemonid == 33){
+       return image033
+    }
+   else if(pokemonid == 34){
+       return image034
+    }
+   else if(pokemonid == 35){
+       return image035
+    }
+   else if(pokemonid == 36){
+       return image036
+    }
+   else if(pokemonid == 37){
+       return image037
+    }
+   else if(pokemonid == 38){
+       return image038
+    }
+   else if(pokemonid == 39){
+       return image039
+    }
+   else if(pokemonid == 40){
+       return image040
+    }
+   else if(pokemonid == 41){
+       return image041
+    }
+   else if(pokemonid == 42){
+       return image042
+    }
+   else if(pokemonid == 43){
+       return image043
+    }
+   else if(pokemonid == 44){
+       return image044
+    }
+   else if(pokemonid == 45){
+       return image045
+    }
+   else if(pokemonid == 46){
+       return image046
+    }
+   else if(pokemonid == 47){
+       return image047
+    }
+   else if(pokemonid == 48){
+       return image048
+    }
+   else if(pokemonid == 49){
+       return image049
+    }
+   else if(pokemonid == 50){
+       return image050
+    }
+   else if(pokemonid == 51){
+       return image051
+    }
+   else if(pokemonid == 52){
+       return image052
+    }
+   else if(pokemonid == 53){
+       return image053
+    }
+   else if(pokemonid == 54){
+       return image054
+    }
+   else if(pokemonid == 55){
+       return image055
+    }
+   else if(pokemonid == 56){
+       return image056
+    }
+   else if(pokemonid == 57){
+       return image057
+    }
+   else if(pokemonid == 58){
+       return image058
+    }
+   else if(pokemonid == 59){
+       return image059
+    }
+   else if(pokemonid == 60){
+       return image060
+    }
+   else if(pokemonid == 61){
+       return image061
+    }
+   else if(pokemonid == 62){
+       return image062
+    }
+   else if(pokemonid == 63){
+       return image063
+    }
+   else if(pokemonid == 64){
+       return image064
+    }
+   else if(pokemonid == 65){
+       return image065
+    }
+   else if(pokemonid == 66){
+       return image066
+    }
+   else if(pokemonid == 67){
+       return image067
+    }
+   else if(pokemonid == 68){
+       return image068
+    }
+   else if(pokemonid == 69){
+       return image069
+    }
+   else if(pokemonid == 70){
+       return image070
+    }
+   else if(pokemonid == 71){
+       return image071
+    }
+   else if(pokemonid == 72){
+       return image072
+    }
+   else if(pokemonid == 73){
+       return image073
+    }
+   else if(pokemonid == 74){
+       return image074
+    }
+   else if(pokemonid == 75){
+       return image075
+    }
+   else if(pokemonid == 76){
+       return image076
+    }
+   else if(pokemonid == 77){
+       return image077
+    }
+   else if(pokemonid == 78){
+       return image078
+    }
+   else if(pokemonid == 79){
+       return image079
+    }
+   else if(pokemonid == 80){
+       return image080
+    }
+   else if(pokemonid == 81){
+       return image081
+    }
+   else if(pokemonid == 82){
+       return image082
+    }
+   else if(pokemonid == 83){
+       return image083
+    }
+   else if(pokemonid == 84){
+       return image084
+    }
+   else if(pokemonid == 85){
+       return image085
+    }
+   else if(pokemonid == 86){
+       return image086
+    }
+   else if(pokemonid == 87){
+       return image087
+    }
+   else if(pokemonid == 88){
+       return image088
+    }
+   else if(pokemonid == 89){
+       return image089
+    }
+   else if(pokemonid == 90){
+       return image090
+    }
+   else if(pokemonid == 91){
+       return image091
+    }
+   else if(pokemonid == 92){
+       return image092
+    }
+   else if(pokemonid == 93){
+       return image093
+    }
+   else if(pokemonid == 94){
+       return image094
+    }
+   else if(pokemonid == 95){
+       return image095
+    }
+   else if(pokemonid == 96){
+       return image096
+    }
+   else if(pokemonid == 97){
+       return image097
+    }
+   else if(pokemonid == 98){
+       return image098
+    }
+   else if(pokemonid == 99){
+       return image099
+    }
+   else if(pokemonid == 100){
+       return image100
+    }
+   else if(pokemonid == 101){
+       return image101
+    }
+   else if(pokemonid == 102){
+       return image102
+    }
+   else if(pokemonid == 103){
+       return image103
+    }
+   else if(pokemonid == 104){
+       return image104
+    }
+   else if(pokemonid == 105){
+       return image105
+    }
+   else if(pokemonid == 106){
+       return image106
+    }
+   else if(pokemonid == 107){
+       return image107
+    }
+   else if(pokemonid == 108){
+       return image108
+    }
+   else if(pokemonid == 109){
+       return image109
+    }
+   else if(pokemonid == 110){
+       return image110
+    }
+   else if(pokemonid == 111){
+       return image111
+    }
+   else if(pokemonid == 112){
+       return image112
+    }
+   else if(pokemonid == 113){
+       return image113
+    }
+   else if(pokemonid == 114){
+       return image114
+    }
+   else if(pokemonid == 115){
+       return image115
+    }
+   else if(pokemonid == 116){
+       return image116
+    }
+   else if(pokemonid == 117){
+       return image117
+    }
+   else if(pokemonid == 118){
+       return image118
+    }
+   else if(pokemonid == 119){
+       return image119
+    }
+   else if(pokemonid == 120){
+       return image120
+    }
+   else if(pokemonid == 121){
+       return image121
+    }
+   else if(pokemonid == 122){
+       return image122
+    }
+   else if(pokemonid == 123){
+       return image123
+    }
+   else if(pokemonid == 124){
+       return image124
+    }
+   else if(pokemonid == 125){
+       return image125
+    }
+   else if(pokemonid == 126){
+       return image126
+    }
+   else if(pokemonid == 127){
+       return image127
+    }
+   else if(pokemonid == 128){
+       return image128
+    }
+   else if(pokemonid == 129){
+       return image129
+    }
+   else if(pokemonid == 130){
+       return image130
+    }
+   else if(pokemonid == 131){
+       return image131
+    }
+   else if(pokemonid == 132){
+       return image132
+    }
+   else if(pokemonid == 133){
+       return image133
+    }
+   else if(pokemonid == 134){
+       return image134
+    }
+   else if(pokemonid == 135){
+       return image135
+    }
+   else if(pokemonid == 136){
+       return image136
+    }
+   else if(pokemonid == 137){
+       return image137
+    }
+   else if(pokemonid == 138){
+       return image138
+    }
+   else if(pokemonid == 139){
+       return image139
+    }
+   else if(pokemonid == 140){
+       return image140
+    }
+   else if(pokemonid == 141){
+       return image141
+    }
+   else if(pokemonid == 142){
+       return image142
+    }
+   else if(pokemonid == 143){
+       return image143
+    }
+   else if(pokemonid == 144){
+       return image144
+    }
+   else if(pokemonid == 145){
+       return image145
+    }
+   else if(pokemonid == 146){
+       return image146
+    }
+   else if(pokemonid == 147){
+       return image147
+    }
+   else if(pokemonid == 148){
+       return image148
+    }
+   else if(pokemonid == 149){
+       return image149
+    }
+   else if(pokemonid == 150){
+       return image150
+    }
+   else if(pokemonid == 151){
+       return image151
+    }
+}
